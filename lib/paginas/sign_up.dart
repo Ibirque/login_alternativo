9b2aa0a245469c1e3e9e2dc4fc75d344e40ccdef
Ibirque <ibirquepardo1@gmail.com>
2024-03-10 07:09:54 +0100

    registro completado

--- lib/paginas/sign_up.dart
+++ lib/paginas/sign_up.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:login_alternativo/componentes/my_buttonsignup.dart';
 import 'package:login_alternativo/componentes/my_textfield.dart';
 import 'package:login_alternativo/componentes/square_tile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:login_alternativo/paginas/login.dart';
 
 class PaginaSignUp extends StatelessWidget {
   PaginaSignUp({super.key});
@@ -14,10 +17,53 @@ class PaginaSignUp extends StatelessWidget {
   final TextEditingController confirmEmailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController = TextEditingController();
-  
 
   // Método para iniciar sesión
-  void registrarUsuario() async {}
+  Future<bool> registrarUsuario(
+    String username,
+    String apellido,
+    String cip,
+    String email,
+    String confirmPassword,
+  ) async {    
+    try {
+      // Obtener los valores de los controladores de texto
+      String username = usernameController.text;
+      String apellido = apellidoControler.text;
+      String cip = cipController.text;
+      String email = emailController.text;
+      String password = passwordController.text;
+
+      // Estructurar los datos
+      Map<String, dynamic> userData = {
+        'username': username,
+        'apellido': apellido,
+        'cip': cip,
+        'email': email,
+        // Agregar otros campos aquí si es necesario
+      };
+
+      // Crear una cuenta de usuario en Firebase Auth
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Guardar los datos en la base de datos
+      FirebaseFirestore.instance
+          .collection('usuarios')
+          .doc(email)
+          .set(userData);
+
+      // Registro exitoso, puedes navegar a otra página o mostrar un mensaje de éxito
+      return true;
+    } catch (e) {
+      // Manejar cualquier error
+      print('Error al registrar el usuario: $e');
+      return false;
+    }
+  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -80,27 +126,27 @@ class PaginaSignUp extends StatelessWidget {
                       MyTextField(
                         controller: apellidoControler,
                         hintText: 'Apellido',
-                        obscureText: true,
+                        obscureText: false,
                       ),
 
                       const SizedBox(height: 10),
                       MyTextField(
                         controller: cipController,
                         hintText: 'CIP Sanitario',
-                        obscureText: true,
+                        obscureText: false,
                       ),
 
                       const SizedBox(height: 10),
                       MyTextField(
                         controller: emailController,
                         hintText: 'e-mail',
-                        obscureText: true,
+                        obscureText: false,
                       ),
                       const SizedBox(height: 10),
                       MyTextField(
                         controller: confirmEmailController,
                         hintText: 'Confirmar e-mail',
-                        obscureText: true,
+                        obscureText: false,
                       ),
 
                       //Campo para password
@@ -121,7 +167,25 @@ class PaginaSignUp extends StatelessWidget {
                       const SizedBox(height: 5),
 
                       MyButton(
-                        onPressed: registrarUsuario,
+                        onPressed: () async {
+                          bool success = await registrarUsuario(
+                            usernameController.text,
+                            apellidoControler.text,
+                            cipController.text,
+                            emailController.text,
+                            confirmPasswordController.text,
+                          );
+                          if (success) {
+                            // ignore: use_build_context_synchronously
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => PaginaLogin()),
+                            );
+                          } else {
+                            // fallo de registro
+                          }
+                        },
                       ),
 
                       //O continuar con:
