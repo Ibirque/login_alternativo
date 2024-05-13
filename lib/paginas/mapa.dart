15088a1d7d92ef00500638ea2d243a8085883e54
Ibirque <ibirquepardo1@gmail.com>
2024-05-13 12:54:25 +0200

    Google places
    Falta añadir filtro de farmacias

--- lib/paginas/mapa.dart
+++ lib/paginas/mapa.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart'; // Importa geolocator
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_webservice/places.dart';
 import 'package:login_alternativo/componentes/bottom_navigation_bar.dart';
+
+//Guia de implementacion de
+// https://fredrick-m.medium.com/google-places-api-with-flutter-ebac822a7546
 
 class MapsPage extends StatefulWidget {
   const MapsPage({Key? key}) : super(key: key);
@@ -13,13 +17,74 @@ class MapsPage extends StatefulWidget {
 class _MapsPageState extends State<MapsPage> {
   late GoogleMapController mapController;
   int _currentIndex = 3;
-
-  LatLng _center = LatLng(0, 0); // Ubicación inicial por defecto
+  LatLng _center = LatLng(41.389262, 2.168451);
+  List<Widget> citasWidgets = [];
+  List<PlacesSearchResult> _placesList = [];
+  final _places =
+      GoogleMapsPlaces(apiKey: 'AIzaSyBf7dMajiznsKDFN2nQOmb4Hq5MCYskYfA');
 
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation(); // Obtener ubicación actual al iniciar la página
+
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition();
+      _updateMapPosition(position);
+
+      citasWidgets.clear();
+      citasWidgets.add(
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 13.0,
+            ),
+            myLocationEnabled: true,
+            compassEnabled: true,
+            mapToolbarEnabled: true,
+            tiltGesturesEnabled: true,
+            myLocationButtonEnabled: true,
+            zoomControlsEnabled: true,
+          ),
+        ),
+      );
+      _searchNearbyPlaces("Farmacia", _center);
+    } catch (e) {
+      print("Error getting current location: $e");
+    }
+  }
+
+  Future<void> _searchNearbyPlaces(String query, LatLng location) async {
+    try {
+      final result = await _places.searchNearbyWithRadius(
+        Location(lat: location.latitude, lng: location.longitude),
+        5000,
+        type: "pharmacy",
+        keyword: query,
+      );
+      if (result.status == "OK") {
+        setState(() {
+          _placesList = result.results;
+        });
+      } else {
+        throw Exception(result.errorMessage);
+      }
+    } catch (e) {
+      print("Error searching nearby places: $e");
+    }
+  }
+
+  void _updateMapPosition(Position position) {
+    setState(() {
+      _center = LatLng(position.latitude, position.longitude);
+    });
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -28,30 +93,26 @@ class _MapsPageState extends State<MapsPage> {
     });
   }
 
-  // Método para obtener la ubicación actual
-  void _getCurrentLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    setState(() {
-      _center = LatLng(position.latitude, position.longitude);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Farmacias cercanas'),
+        title: Text(
+          'Farmacias cercanas',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+        ),
+        centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 11.0,
-        ),
+      // Muestra los widgets almacenados en la lista
+      body: Stack(
+        children: citasWidgets,
       ),
-
+      
       bottomNavigationBar: MyBottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
